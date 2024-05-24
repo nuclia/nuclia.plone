505de351899f76b10803f2ee4eb357b8cdb66aa7
Eric BREHAULT <ebrehault@gmail.com>
2024-05-24 10:12:38 +0200

    Back to development: 1.0b2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='nuclia.plone',
-    version='1.0b1',
+    version='1.0b2.dev0',
     description="Integrate Nuclia search in Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
